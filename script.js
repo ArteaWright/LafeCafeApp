38ffd2547921003b10c992efa6075566ea1c66ae
Artea Wright <artealwright@gmail.com>
2020-11-05 12:10:08 -0500

    add form validation

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function order() {
-let u = document.forms["myform"]["flname"].value;
-let v = document.forms["myform"]["email"].value;
-let y = document.getElementById("coffee").selectedIndex;
-let z = document.getElementsByTagName("option")[y].value;
-let s = document.getElementById("size").selectedIndex;
-let t = document.getElementsByTagName("option")[s].value;
-let w = document.forms["myform"]["date"].value;
-let x = document.forms["myform"]["time"].value;
-if (u, v, z, t, w, x === "") {
+function validate(e) {
+    let u = document.forms["myform"]["flname"].value;
+    let v = document.forms["myform"]["email"].value;
+    let y = document.getElementById("coffee").selectedIndex;
+    let z = document.getElementsByTagName("option")[y].value;
+    let s = document.getElementById("size").selectedIndex;
+    let t = document.getElementsByTagName("option")[s].value;
+    let w = document.forms["myform"]["date"].value;
+    let x = document.forms["myform"]["time"].value;
+    if (u, v, z, t, w, x === "") {
     alert("please fill");
-} 
+    return false;
+}
 else {
     document.getElementById("confirmation").style.display = "block";
     document.getElementById("button").style.display = "none";
+    }
 }
-}
-
