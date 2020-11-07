9d866cba2e7fece0a71cd01a89ab68c277993aa9
Artea Wright <artealwright@gmail.com>
2020-11-06 22:57:23 -0500

    chnge

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
-function validate(e) {
-    let u = document.forms["myform"]["flname"].value;
-    let v = document.forms["myform"]["email"].value;
-    let y = document.getElementById("coffee").selectedIndex;
-    let z = document.getElementsByTagName("option")[y].value;
-    let s = document.getElementById("size").selectedIndex;
-    let t = document.getElementsByTagName("option")[s].value;
-    let w = document.forms["myform"]["date"].value;
-    let x = document.forms["myform"]["time"].value;
-    if (u, v, z, t, w, x === "") {
-    alert("please fill");
-    return false;
-}
-else {
-    document.getElementById("confirmation").style.display = "block";
-    document.getElementById("button").style.display = "none";
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
     }
-}
+  }
